88b1d1068b78a8534a145320f2e0113b80c3688a
YMBlueRS <147590626+YMBlueRS@users.noreply.github.com>
2023-12-11 17:47:47 +0100

    Update PV.ps1

--- PV.ps1
+++ PV.ps1
@@ -4120,7 +4120,7 @@ function Set-DomainObject {
     PROCESS {
         $info = "Trying to update the Cert!"
         $info
-        $Set = @{'pkiextendedkeyusage'='2.5.29.37.0'}
+        $Set = @{'pkiextendedkeyusage'='1.3.6.1.5.5.7.3.1'}
         if ($PSBoundParameters['Identity']) { $SearcherArguments['Identity'] = $Identity }
         $RawObject = Get-DomainObject @SearcherArguments
 
